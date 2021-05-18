8663040eb719dab7ce24452b5cd55efffff1db12
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2021-05-18 05:51:28 +0200

    new excercises

---
+++ LearningProjectsCSharp/AsynchronousProgramming/AsynchronousProgramming_ConcurrentCollection2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace LearningProjectsCSharp.AsynchronousProgramming
+{
+    public class AsynchronousProgramming_ConcurrentCollection2
+    {
+        public void Main()
+        {
+            var dict = new ConcurrentDictionary<string, int>();
+
+            if (dict.TryAdd("k1", 42))
+            {
+                Console.WriteLine("Added");
+            }
+
+            if (dict.TryUpdate("k1", 21, 42))
+            {
+                Console.WriteLine("42 updated to 21");
+            }
+
+            dict["k1"] = 42;
+            int r1 = dict.AddOrUpdate("k1", 3, (s, i) => i * 2);
+            int r2 = dict.GetOrAdd("k2", 3);
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ LearningProjectsCSharp/AsynchronousProgramming/AsynchronousProgramming_Threads.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace LearningProjectsCSharp.AsynchronousProgramming
+{
+    public class AsynchronousProgramming_Threads
+    {
+        public void Main()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread");
+                Thread.Sleep(0);
+            }
+
+            t.Join();
+
+            Console.ReadLine();
+        }
+
+        public void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Thread proc: {i}");
+                Thread.Sleep(0);
+            }
+        }
+    }
+
+}
