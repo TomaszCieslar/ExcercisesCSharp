1065be2646478f916467a7a9a206750d5e0e16b7
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2021-05-18 05:42:44 +0200

    Add project files.

---
+++ LearningProjectsCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningProjectsCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
